ffd1aeb8b08f804d452cf46259213ec81a64d673
AndreyBykov <aabykov@gmail.com>
2022-07-20 23:58:29 +0200

    add playwright-core and puppeteer deps for ts e2e test

--- test/e2e/tools.mjs
+++ test/e2e/tools.mjs
@@ -135,6 +135,7 @@ async function copyPackages(dirName) {
     // We don't need to copy the following packages
     delete dependencies['@apify/storage-local'];
     delete dependencies['deep-equal'];
+    delete dependencies['playwright-core'];
     delete dependencies.apify;
     delete dependencies.puppeteer;
     delete dependencies.playwright;
